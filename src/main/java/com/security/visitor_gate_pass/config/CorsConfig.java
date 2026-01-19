c69ff6a176745739c54dba7cff8652685321d74e
Ragul M <ragulm12a1@gmail.com>
2026-01-19 10:16:59 +0530

    Backend logs

--- src/main/java/com/security/visitor_gate_pass/config/CorsConfig.java
+++ src/main/java/com/security/visitor_gate_pass/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","https://visitor-gate-pass-rwji.vercel.app/login")
+                        .allowedOrigins("http://localhost:5173", "https://visitor-gate-pass-rwji.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
